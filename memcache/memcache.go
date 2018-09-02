4ee31dc2e4ced64e38f8094c39b80dd3188226e4
themihai <mihai@epek.com>
2018-09-02 15:23:35 +0300

    feat: support custom dialer
    From https://github.com/bradfitz/gomemcache/pull/86

--- memcache/memcache.go
+++ memcache/memcache.go
@@ -132,6 +132,8 @@ func NewFromSelector(ss ServerSelector) *Client {
 // Client is a memcache client.
 // It is safe for unlocked use by multiple concurrent goroutines.
 type Client struct {
+	// Dialer specifies a custom dialer used to dial new connections to a server.
+	DialTimeout func(network, address string, timeout time.Duration) (net.Conn, error)
 	// Timeout specifies the socket read/write timeout.
 	// If zero, DefaultTimeout is used.
 	Timeout time.Duration
@@ -255,7 +257,10 @@ func (cte *ConnectTimeoutError) Error() string {
 }
 
 func (c *Client) dial(addr net.Addr) (net.Conn, error) {
-	nc, err := net.DialTimeout(addr.Network(), addr.String(), c.netTimeout())
+	if c.DialTimeout == nil {
+		c.DialTimeout = net.DialTimeout
+	}
+	nc, err := c.DialTimeout(addr.Network(), addr.String(), c.netTimeout())
 	if err == nil {
 		return nc, nil
 	}
