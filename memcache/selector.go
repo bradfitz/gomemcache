831eb95f27df5934d3489f7f8f15adebea3d5b1a
Tim Ruffles <truffles@plaid.com>
2021-03-16 10:23:58 +0000

    implement circuit breaking

--- memcache/selector.go
+++ memcache/selector.go
@@ -17,10 +17,14 @@ limitations under the License.
 package memcache
 
 import (
+	"errors"
+	"fmt"
 	"hash/crc32"
+	"io"
 	"net"
 	"strings"
 	"sync"
+	"time"
 )
 
 // ServerSelector is the interface that selects a memcache server
@@ -32,14 +36,62 @@ type ServerSelector interface {
 	// PickServer returns the server address that a given item
 	// should be shared onto.
 	PickServer(key string) (net.Addr, error)
+	// Each iterates func over servers, returning the first non-nil error
 	Each(func(net.Addr) error) error
+	// OnResult informs the selector of the result of an operation - err is nil on success
+	OnResult(addr net.Addr, err error)
 }
 
-// ServerList is a simple ServerSelector. Its zero value is usable.
-type ServerList struct {
-	mu    sync.RWMutex
-	addrs []net.Addr
+const maxRetryWait = time.Second * 10
+const startingWait = time.Millisecond * 10
+
+// retryState drives the per connection state-machine through the retry process
+type retryState string
+
+const (
+	// kept unavailable until ready for retry
+	retryWait retryState = "RetryWait"
+	// ready to be picked
+	retryReady retryState = "RetryReady"
+	// address is having a single operation run against it to see if sever has come back online
+	retryRunning retryState = "RetryRunning"
+)
+
+type waitState struct {
+	addr net.Addr
+	wait time.Duration
+	retry retryState
 }
+
+// serversWithBreaker is a ServerSelector with circuit-breaking
+type serversWithBreaker struct {
+	// protects the available and states fields
+	mu sync.Mutex
+
+	// addrs - not mutated after construction
+	addrs []net.Addr
+
+	// optimization: the presence of this field allows us to avoid allocations in PickServer, while preserving the
+	// ability to load balance with duplicate addrs
+	available []net.Addr
+	// records the waitState of servers that have had connection issues. This is
+	// used in filterAvailable to decide which servers are currently considered
+	// available for connection
+	states map[net.Addr]waitState
+}
+
+// NewSelectorWithBreaker will resolve all addrs once, and return
+// a selector list that takes addresses out of the list on error.
+// Addresses are retried with an exponential backoff.
+func NewSelectorWithBreaker(addrs []string) (ServerSelector, error) {
+	ss := new(serversWithBreaker)
+	if err := ss.resolveServers(addrs...); err != nil {
+		return nil, err
+	}
+	return ss, nil
+}
+
+var _ ServerSelector = &serversWithBreaker{}
 
 // staticAddr caches the Network() and String() values from any net.Addr.
 type staticAddr struct {
@@ -56,16 +108,9 @@ func newStaticAddr(a net.Addr) net.Addr {
 func (s *staticAddr) Network() string { return s.ntw }
 func (s *staticAddr) String() string  { return s.str }
 
-// SetServers changes a ServerList's set of servers at runtime and is
-// safe for concurrent use by multiple goroutines.
-//
-// Each server is given equal weight. A server is given more weight
-// if it's listed multiple times.
-//
-// SetServers returns an error if any of the server names fail to
-// resolve. No attempt is made to connect to the server. If any error
-// is returned, no changes are made to the ServerList.
-func (ss *ServerList) SetServers(servers ...string) error {
+// resolveServers returns an error if any of the server names fail to
+// resolve. No attempt is made to connect to the server.
+func (ss *serversWithBreaker) resolveServers(servers ...string) error {
 	naddr := make([]net.Addr, len(servers))
 	for i, server := range servers {
 		if strings.Contains(server, "/") {
@@ -86,13 +131,13 @@ func (ss *ServerList) SetServers(servers ...string) error {
 	ss.mu.Lock()
 	defer ss.mu.Unlock()
 	ss.addrs = naddr
+	ss.available = make([]net.Addr, 0, len(ss.addrs))
+	ss.filterAvailable()
 	return nil
 }
 
-// Each iterates over each server calling the given function
-func (ss *ServerList) Each(f func(net.Addr) error) error {
-	ss.mu.RLock()
-	defer ss.mu.RUnlock()
+// Each iterates over each server, regardless of current availability
+func (ss *serversWithBreaker) Each(f func(net.Addr) error) error {
 	for _, a := range ss.addrs {
 		if err := f(a); nil != err {
 			return err
@@ -111,19 +156,130 @@ var keyBufPool = sync.Pool{
 	},
 }
 
-func (ss *ServerList) PickServer(key string) (net.Addr, error) {
-	ss.mu.RLock()
-	defer ss.mu.RUnlock()
-	if len(ss.addrs) == 0 {
+func (ss *serversWithBreaker) PickServer(key string) (picked net.Addr, err error) {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+
+	// if we pick a connection for retry, remove it from the available pool till we
+	// hear the result
+	defer (func() {
+		if err != nil {
+			return
+		}
+		// remove the address from available roster again to avoid multiple retries
+		if ws, ok := ss.states[picked]; ok && ws.retry == retryReady {
+			ws.retry = retryRunning
+			ss.setState(picked, ws)
+		}
+	})()
+
+	if len(ss.available) == 0 {
 		return nil, ErrNoServers
 	}
-	if len(ss.addrs) == 1 {
-		return ss.addrs[0], nil
+	if len(ss.available) == 1 {
+		return ss.available[0], nil
 	}
 	bufp := keyBufPool.Get().(*[]byte)
 	n := copy(*bufp, key)
 	cs := crc32.ChecksumIEEE((*bufp)[:n])
 	keyBufPool.Put(bufp)
 
-	return ss.addrs[cs%uint32(len(ss.addrs))], nil
+	return ss.available[cs%uint32(len(ss.available))], nil
+}
+
+func (ss *serversWithBreaker) OnResult(addr net.Addr, err error) {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+
+	if err == nil || !isConnectionError(err) {
+		// server is considered available once we successfully
+		// communicate
+		if _, ok := ss.states[addr]; ok {
+			ss.deleteState(addr)
+		}
+		return
+	}
+
+	ws := waitState{
+		addr:  addr,
+		wait:  startingWait,
+		retry: retryWait,
+	}
+	if st, ok := ss.states[addr]; ok {
+		// We've already registered an error. Since we use connections concurrently
+		// we want to avoid double backing-off (and double retrying) here.
+		switch st.retry {
+		case retryWait:
+			// waiting for retry to be scheduled
+			return
+		case retryReady:
+			// scheduled, waiting to be picked
+			return
+		case retryRunning:
+			// backoff: we're here because we hit an error during the retry
+			ws = st
+			ws.wait *= 2
+			ws.retry = retryWait
+			if ws.wait > maxRetryWait {
+				ws.wait = maxRetryWait
+			}
+		default:
+			panic(fmt.Errorf("unexpected retry state: %s", st.retry))
+		}
+	}
+	ss.setState(addr, ws)
+
+	if ws.retry == retryWait {
+		go ss.scheduleRetry(addr, ws.wait)
+	}
+}
+
+// Sets the retry state for an address, and updates the available list.
+// MUST be called from a method with a lock on the mutex, or will cause concurrent map crashes
+func (ss *serversWithBreaker) setState(addr net.Addr, ws waitState) {
+	if ss.states == nil {
+		ss.states = map[net.Addr]waitState{}
+	}
+	ss.states[addr] = ws
+	ss.filterAvailable()
+}
+
+// MUST be called from a method with a lock on the mutex, or will cause concurrent map crashes
+func (ss *serversWithBreaker) deleteState(addr net.Addr) {
+	delete(ss.states, addr)
+	ss.filterAvailable()
+}
+
+func (ss *serversWithBreaker) filterAvailable() {
+	// start with nothing available, and add available servers (with duplicates for balancing)
+	ss.available = ss.available[:0]
+	for _, addr := range ss.addrs {
+		if ws, ok := ss.states[addr]; !ok || ws.retry == retryReady {
+			ss.available = append(ss.available, addr)
+		}
+	}
+}
+
+// is a network error, rather than a protocol level error like ErrCacheMiss
+func isConnectionError(err error) bool {
+	var netErr net.Error
+	return errors.As(err, &netErr) || errors.Is(err, io.EOF)
+}
+
+func (ss *serversWithBreaker) scheduleRetry(addr net.Addr, wait time.Duration) {
+	time.Sleep(wait)
+
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	ws, ok := ss.states[addr]
+	if !ok {
+		// recovered while routine was sleeping
+		return
+	}
+	if ws.retry != retryWait {
+		panic(fmt.Errorf("retry unexpectedly not in RETRY_WAIT: %s", ws.retry))
+	}
+	// schedule a retry
+	ws.retry = retryReady
+	ss.setState(addr, ws)
 }
--- memcache/selector_test.go
+++ memcache/selector_test.go
@@ -16,24 +16,272 @@ limitations under the License.
 
 package memcache
 
-import "testing"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net"
+	"os/exec"
+	"sync"
+	"testing"
+	"time"
+)
 
-func BenchmarkPickServer(b *testing.B) {
+func BenchmarkPickServerWithBreaker(b *testing.B) {
 	// at least two to avoid 0 and 1 special cases:
 	benchPickServer(b, "127.0.0.1:1234", "127.0.0.1:1235")
 }
 
-func BenchmarkPickServer_Single(b *testing.B) {
+func BenchmarkPickServer_SingleWithBreaker(b *testing.B) {
 	benchPickServer(b, "127.0.0.1:1234")
 }
 
 func benchPickServer(b *testing.B, servers ...string) {
 	b.ReportAllocs()
-	var ss ServerList
-	ss.SetServers(servers...)
+	var ss serversWithBreaker
+	requireNoError(b, ss.resolveServers(servers...))
 	for i := 0; i < b.N; i++ {
 		if _, err := ss.PickServer("some key"); err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+// Note: this test is technically non-deterministic as the retry scheduling is async,
+// but failures should be exceedingly rare due to the length of time until the first timeout.
+//
+// If a fix is required: probably simplest to change the startingWait to a var and modify in this test
+func TestBehaviourSynchronously(t *testing.T) {
+	create := func(t *testing.T) (ServerSelector, net.Addr, net.Addr) {
+		ss, err := NewSelectorWithBreaker([]string{"127.0.0.1:4200", "127.255.0.1:4200"})
+		requireNoError(t, err)
+		wb := ss.(*serversWithBreaker)
+		addrA := wb.addrs[0]
+		addrB := wb.addrs[1]
+		if addrA == addrB {
+			t.Fatalf("for test to be valid addresses must differ")
+		}
+		return ss, addrA, addrB
+	}
+
+	t.Run("after a net error, an address should be not be pickable until retried", func(t *testing.T) {
+		ss, addrA, _ := create(t)
+
+		ss.OnResult(addrA, &net.OpError{})
+		fuzzer(func(k string) {
+			addr, err := ss.PickServer(k)
+			requireNoError(t, err)
+			if addrA == addr {
+				t.Fatalf("address still available")
+			}
+		})
+	})
+
+	t.Run("after a successful retry, an address should be available again", func(t *testing.T) {
+		ss, addrA, _ := create(t)
+		ss.OnResult(addrA, nil)
+		gotAddr := 0
+		fuzzer(func(k string) {
+			addr, err := ss.PickServer(k)
+			requireNoError(t, err)
+			if addr == addrA {
+				gotAddr++
+			}
+		})
+		if gotAddr == 0 {
+			t.Fatalf("address not pickable again")
+		}
+	})
+
+	t.Run("after a net error, an address should be removed", func(t *testing.T) {
+		ss, addrA, addrB := create(t)
+		// when all address have errors, we return an error that signals no servers are available (circuit breaker)
+		ss.OnResult(addrA, &net.OpError{})
+		ss.OnResult(addrB, &net.OpError{})
+
+		_, err := ss.PickServer("hi")
+		assertEqualError(t, err, "memcache: no servers configured or available")
+	})
+}
+
+func TestRetriesAfterError(t *testing.T) {
+	ss := createWithServers(t, []string{"127.0.0.1:1234"})
+
+	addrA := ss.addrs[0]
+	ss.OnResult(addrA, &net.OpError{})
+	_, err := ss.PickServer("hi")
+	assertEqualError(t, err, "memcache: no servers configured or available")
+
+	time.Sleep(startingWait * 2)
+	_, err = ss.PickServer("hi")
+	// should have become available again
+	requireNoError(t, err)
+}
+
+func TestRecoversOnSuccessfulCommunication(t *testing.T) {
+	ss := createWithServers(t, []string{"127.0.0.1:1234"})
+	addrA := ss.addrs[0]
+
+	ss.OnResult(addrA, &net.OpError{})
+	_, err := ss.PickServer("hi")
+	assertEqualError(t, err, "memcache: no servers configured or available")
+
+	ss.OnResult(addrA, nil)
+	_, err = ss.PickServer("hi")
+	// should have become available again
+	requireNoError(t, err)
+}
+
+func TestOneServer(t *testing.T) {
+	functionalTest(t, []string{"42111"})
+}
+
+func TestTwoServers(t *testing.T) {
+	functionalTest(t, []string{"42111", "42222"})
+}
+
+// This is a functional test that spins up and repeatedly takes down a memcached node.
+func functionalTest(t *testing.T, ports []string) {
+	srvs := []string{}
+	for _, port := range ports {
+		srvs = append(srvs, fmt.Sprintf("localhost:%s", port))
+	}
+	cli := New(srvs...)
+
+	errCounts := map[string]int{}
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+	networkErrorCategory := "[network error]"
+	internalTimeoutError := "[memcachepl timeout]"
+
+	server := func(port string) {
+		time.Sleep(time.Millisecond * time.Duration(rand.Int31n(25)))
+		for i := 0; i < 5; i++ {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+			defer cancel()
+			cmd := exec.CommandContext(ctx, "memcached", "-p", port)
+			err := cmd.Run()
+			assertEqualError(t, err, "signal: killed")
+			time.Sleep(time.Millisecond*500 + time.Duration(rand.Int31n(50)))
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(srvs))
+	for _, port := range ports {
+		go server(port)
+	}
+
+	go (func() {
+		wg.Wait()
+		close(done)
+	})()
+
+	mx := sync.Mutex{}
+	writer := func() {
+		for {
+			select {
+			case <-done:
+				break
+			default:
+				err := cli.Set(&Item{
+					Key:   "hi",
+					Value: []byte("val"),
+				})
+				errString := fmt.Sprintf("%v", err)
+				var netErr net.Error
+				if errors.As(err, &netErr) {
+					errString = networkErrorCategory
+				}
+				var cte *ConnectTimeoutError
+				if errors.As(err, &cte) {
+					errString = internalTimeoutError
+				}
+				mx.Lock()
+				errCounts[errString]++
+				mx.Unlock()
+			}
+			time.Sleep(time.Duration(rand.Int31n(5)) * time.Millisecond)
+		}
+	}
+
+	go writer()
+	go writer()
+	go writer()
+	go writer()
+	go writer()
+	go writer()
+
+	wg.Wait()
+
+	mx.Lock()
+	defer mx.Unlock()
+
+	unexpectedErrors := map[string]struct{}{}
+	total := 0
+	for errVal, count := range errCounts {
+		total += count
+		switch errVal {
+
+		// filter out expected errors
+		case "<nil>":
+		case "EOF":
+		case internalTimeoutError:
+		case networkErrorCategory:
+		case ErrNoServers.Error():
+
+		default:
+			unexpectedErrors[errVal] = struct{}{}
+		}
+	}
+
+	// we're up for approximately 2.5 seconds out of every three, so we should have a lot of successes too
+	assertGreaterOrEqual(t, errCounts["<nil>"], total/10, "expected many successes")
+
+	for s := range unexpectedErrors {
+		t.Errorf("unexpected errors during test - either indicates a bug or something that needs categorisation: %s", s)
+	}
+
+	// test that we have greatly more breaker errors than network errors, reducing the reconnection by a large factor
+	networkFails := errCounts[networkErrorCategory] + errCounts[internalTimeoutError] +
+		errCounts["EOF"]
+	assertGreaterOrEqual(t, errCounts[ErrNoServers.Error()], networkFails*10, "didn't reduce reconnection load as expected")
+}
+
+func fuzzer(withKey func(s string)) {
+	rand.Seed(42)
+	for i := 0; i < 100; i++ {
+		withKey(fmt.Sprintf("key%d", rand.Int63()))
+	}
+}
+
+func createWithServers(t testing.TB, srvs []string) *serversWithBreaker {
+	ss, err := NewSelectorWithBreaker(srvs)
+	requireNoError(t, err)
+	return ss.(*serversWithBreaker)
+}
+
+
+func requireNoError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error %s", err)
+	}
+}
+
+func assertEqualError(t testing.TB, err error, msg string) {
+	t.Helper()
+	if err == nil {
+		t.Errorf("expected error")
+	} else if err.Error() != msg {
+		t.Errorf("expected error %q got %q", msg, err.Error())
+	}
+}
+
+func assertGreaterOrEqual(t testing.TB, expected, val int, msg string) {
+	t.Helper()
+	if val < expected {
+		t.Errorf("%s: expected >= %d, got %d", msg, expected, val)
 	}
 }
