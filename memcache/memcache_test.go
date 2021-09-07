501918b8ee9dc0213f76f9f2390ec6111b406977
Artjom Kurapov <artkurapov@gmail.com>
2021-09-07 21:07:42 +0300

    use more explicit hierarchy of tests

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -18,16 +18,16 @@ limitations under the License.
 package memcache
 
 import (
-"bufio"
-"fmt"
-"io"
-"io/ioutil"
-"net"
-"os"
-"os/exec"
-"strings"
-"testing"
-"time"
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
 )
 
 const testServer = "localhost:11211"
@@ -87,131 +87,143 @@ func testWithClient(t *testing.T, c *Client) {
 	}
 	mustSet := mustSetF(t, c)
 
-	// Set
-	foo := &Item{Key: "foo", Value: []byte("fooval"), Flags: 123}
-	err := c.Set(foo)
-	checkErr(err, "first set(foo): %v", err)
-	err = c.Set(foo)
-	checkErr(err, "second set(foo): %v", err)
+	t.Run("Set", func(t *testing.T) {
+		foo := &Item{Key: "foo", Value: []byte("fooval"), Flags: 123}
+		err := c.Set(foo)
+		checkErr(err, "first set(foo): %v", err)
+		err = c.Set(foo)
+		checkErr(err, "second set(foo): %v", err)
+	})
 
-	// Get
-	it, err := c.Get("foo")
-	checkErr(err, "get(foo): %v", err)
-	if it.Key != "foo" {
-		t.Errorf("get(foo) Key = %q, want foo", it.Key)
-	}
-	if string(it.Value) != "fooval" {
-		t.Errorf("get(foo) Value = %q, want fooval", string(it.Value))
-	}
-	if it.Flags != 123 {
-		t.Errorf("get(foo) Flags = %v, want 123", it.Flags)
-	}
+	t.Run("Get", func(t *testing.T) {
+		it, err := c.Get("foo")
+		checkErr(err, "get(foo): %v", err)
+		if it.Key != "foo" {
+			t.Errorf("get(foo) Key = %q, want foo", it.Key)
+		}
+		if string(it.Value) != "fooval" {
+			t.Errorf("get(foo) Value = %q, want fooval", string(it.Value))
+		}
+		if it.Flags != 123 {
+			t.Errorf("get(foo) Flags = %v, want 123", it.Flags)
+		}
+	})
 
-	// Get and set a unicode key
-	quxKey := "Hello_世界"
-	qux := &Item{Key: quxKey, Value: []byte("hello world")}
-	err = c.Set(qux)
-	checkErr(err, "first set(Hello_世界): %v", err)
-	it, err = c.Get(quxKey)
-	checkErr(err, "get(Hello_世界): %v", err)
-	if it.Key != quxKey {
-		t.Errorf("get(Hello_世界) Key = %q, want Hello_世界", it.Key)
-	}
-	if string(it.Value) != "hello world" {
-		t.Errorf("get(Hello_世界) Value = %q, want hello world", string(it.Value))
-	}
+	t.Run("Get and set a unicode key", func(t *testing.T) {
+		quxKey := "Hello_世界"
+		qux := &Item{Key: quxKey, Value: []byte("hello world")}
+		err := c.Set(qux)
+		checkErr(err, "first set(Hello_世界): %v", err)
+		it, err := c.Get(quxKey)
+		checkErr(err, "get(Hello_世界): %v", err)
+		if it.Key != quxKey {
+			t.Errorf("get(Hello_世界) Key = %q, want Hello_世界", it.Key)
+		}
+		if string(it.Value) != "hello world" {
+			t.Errorf("get(Hello_世界) Value = %q, want hello world", string(it.Value))
+		}
+	})
 
-	// Set malformed keys
-	malFormed := &Item{Key: "foo bar", Value: []byte("foobarval")}
-	err = c.Set(malFormed)
-	if err != ErrMalformedKey {
-		t.Errorf("set(foo bar) should return ErrMalformedKey instead of %v", err)
-	}
-	//malFormed = &Item{Key: "foo" + string(0x7f), Value: []byte("foobarval")}
-	//err = c.Set(malFormed)
-	//if err != ErrMalformedKey {
-	//	t.Errorf("set(foo<0x7f>) should return ErrMalformedKey instead of %v", err)
-	//}
+	t.Run("Set malformed keys", func(t *testing.T) {
+		malFormed := &Item{Key: "foo bar", Value: []byte("foobarval")}
+		err := c.Set(malFormed)
+		if err != ErrMalformedKey {
+			t.Errorf("set(foo bar) should return ErrMalformedKey instead of %v", err)
+		}
+		//malFormed = &Item{Key: "foo" + string(0x7f), Value: []byte("foobarval")}
+		//err = c.Set(malFormed)
+		//if err != ErrMalformedKey {
+		//	t.Errorf("set(foo<0x7f>) should return ErrMalformedKey instead of %v", err)
+		//}
+	})
 
-	// Add
-	bar := &Item{Key: "bar", Value: []byte("barval")}
-	err = c.Add(bar)
-	checkErr(err, "first add(foo): %v", err)
-	if err := c.Add(bar); err != ErrNotStored {
-		t.Fatalf("second add(foo) want ErrNotStored, got %v", err)
-	}
+	t.Run("Add", func(t *testing.T) {
+		bar := &Item{Key: "bar", Value: []byte("barval")}
+		err := c.Add(bar)
+		checkErr(err, "first add(foo): %v", err)
+		if err := c.Add(bar); err != ErrNotStored {
+			t.Fatalf("second add(foo) want ErrNotStored, got %v", err)
+		}
+	})
 
-	// Replace
-	baz := &Item{Key: "baz", Value: []byte("bazvalue")}
-	if err := c.Replace(baz); err != ErrNotStored {
-		t.Fatalf("expected replace(baz) to return ErrNotStored, got %v", err)
-	}
-	err = c.Replace(bar)
-	checkErr(err, "replaced(foo): %v", err)
+	t.Run("Replace", func(t *testing.T) {
+		bar := &Item{Key: "bar", Value: []byte("barval")}
+		baz := &Item{Key: "baz", Value: []byte("bazvalue")}
+		if err := c.Replace(baz); err != ErrNotStored {
+			t.Fatalf("expected replace(baz) to return ErrNotStored, got %v", err)
+		}
+		err := c.Replace(bar)
+		checkErr(err, "replaced(foo): %v", err)
+	})
 
-	// GetMulti
-	m, err := c.GetMulti([]string{"foo", "bar"})
-	checkErr(err, "GetMulti: %v", err)
-	if g, e := len(m), 2; g != e {
-		t.Errorf("GetMulti: got len(map) = %d, want = %d", g, e)
-	}
-	if _, ok := m["foo"]; !ok {
-		t.Fatalf("GetMulti: didn't get key 'foo'")
-	}
-	if _, ok := m["bar"]; !ok {
-		t.Fatalf("GetMulti: didn't get key 'bar'")
-	}
-	if g, e := string(m["foo"].Value), "fooval"; g != e {
-		t.Errorf("GetMulti: foo: got %q, want %q", g, e)
-	}
-	if g, e := string(m["bar"].Value), "barval"; g != e {
-		t.Errorf("GetMulti: bar: got %q, want %q", g, e)
-	}
+	t.Run("GetMulti", func(t *testing.T) {
+		m, err := c.GetMulti([]string{"foo", "bar"})
+		checkErr(err, "GetMulti: %v", err)
+		if g, e := len(m), 2; g != e {
+			t.Errorf("GetMulti: got len(map) = %d, want = %d", g, e)
+		}
+		if _, ok := m["foo"]; !ok {
+			t.Fatalf("GetMulti: didn't get key 'foo'")
+		}
+		if _, ok := m["bar"]; !ok {
+			t.Fatalf("GetMulti: didn't get key 'bar'")
+		}
+		if g, e := string(m["foo"].Value), "fooval"; g != e {
+			t.Errorf("GetMulti: foo: got %q, want %q", g, e)
+		}
+		if g, e := string(m["bar"].Value), "barval"; g != e {
+			t.Errorf("GetMulti: bar: got %q, want %q", g, e)
+		}
+	})
 
-	// Delete
-	err = c.Delete("foo")
-	checkErr(err, "Delete: %v", err)
-	it, err = c.Get("foo")
-	if err != ErrCacheMiss {
-		t.Errorf("post-Delete want ErrCacheMiss, got %v", err)
-	}
+	t.Run("Delete", func(t *testing.T) {
+		err := c.Delete("foo")
+		checkErr(err, "Delete: %v", err)
+		_, err = c.Get("foo")
+		if err != ErrCacheMiss {
+			t.Errorf("post-Delete want ErrCacheMiss, got %v", err)
+		}
+	})
 
-	// Incr/Decr
-	mustSet(&Item{Key: "num", Value: []byte("42")})
-	n, err := c.Increment("num", 8)
-	checkErr(err, "Increment num + 8: %v", err)
-	if n != 50 {
-		t.Fatalf("Increment num + 8: want=50, got=%d", n)
-	}
-	n, err = c.Decrement("num", 49)
-	checkErr(err, "Decrement: %v", err)
-	if n != 1 {
-		t.Fatalf("Decrement 49: want=1, got=%d", n)
-	}
-	err = c.Delete("num")
-	checkErr(err, "delete num: %v", err)
-	n, err = c.Increment("num", 1)
-	if err != ErrCacheMiss {
-		t.Fatalf("increment post-delete: want ErrCacheMiss, got %v", err)
-	}
-	mustSet(&Item{Key: "num", Value: []byte("not-numeric")})
-	n, err = c.Increment("num", 1)
-	if err == nil || !strings.Contains(err.Error(), "client error") {
-		t.Fatalf("increment non-number: want client error, got %v", err)
-	}
-	testTouchWithClient(t, c)
+	t.Run("Incr/Decr", func(t *testing.T) {
+		mustSet(&Item{Key: "num", Value: []byte("42")})
+		n, err := c.Increment("num", 8)
+		checkErr(err, "Increment num + 8: %v", err)
+		if n != 50 {
+			t.Fatalf("Increment num + 8: want=50, got=%d", n)
+		}
+		n, err = c.Decrement("num", 49)
+		checkErr(err, "Decrement: %v", err)
+		if n != 1 {
+			t.Fatalf("Decrement 49: want=1, got=%d", n)
+		}
+		err = c.Delete("num")
+		checkErr(err, "delete num: %v", err)
+		n, err = c.Increment("num", 1)
+		if err != ErrCacheMiss {
+			t.Fatalf("increment post-delete: want ErrCacheMiss, got %v", err)
+		}
+		mustSet(&Item{Key: "num", Value: []byte("not-numeric")})
+		n, err = c.Increment("num", 1)
+		if err == nil || !strings.Contains(err.Error(), "client error") {
+			t.Fatalf("increment non-number: want client error, got %v", err)
+		}
+		testTouchWithClient(t, c)
+	})
 
-	// Test Delete All
-	err = c.DeleteAll()
-	checkErr(err, "DeleteAll: %v", err)
-	it, err = c.Get("bar")
-	if err != ErrCacheMiss {
-		t.Errorf("post-DeleteAll want ErrCacheMiss, got %v", err)
-	}
+	t.Run("Test Delete All", func(t *testing.T) {
+		err := c.DeleteAll()
+		checkErr(err, "DeleteAll: %v", err)
+		_, err = c.Get("bar")
+		if err != ErrCacheMiss {
+			t.Errorf("post-DeleteAll want ErrCacheMiss, got %v", err)
+		}
+	})
 
-	// Test Ping
-	err = c.Ping()
-	checkErr(err, "error ping: %s", err)
+	t.Run("Test Ping", func(t *testing.T) {
+		err := c.Ping()
+		checkErr(err, "error ping: %s", err)
+	})
 }
 
 func testTouchWithClient(t *testing.T, c *Client) {
