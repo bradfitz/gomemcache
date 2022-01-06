1e3230f1d830dd339a0cdaf794065897344d6712
Robert-André Mauchin <30413512+eclipseo@users.noreply.github.com>
2022-01-06 22:53:58 +0100

    Convert to string using rune() (#123)
    See https://github.com/golang/go/issues/32479
    
    Fixes #122.
    
    Signed-off-by: Robert-André Mauchin <zebob.m@gmail.com>

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -127,7 +127,7 @@ func testWithClient(t *testing.T, c *Client) {
 	if err != ErrMalformedKey {
 		t.Errorf("set(foo bar) should return ErrMalformedKey instead of %v", err)
 	}
-	malFormed = &Item{Key: "foo" + string(0x7f), Value: []byte("foobarval")}
+	malFormed = &Item{Key: "foo" + string(rune(0x7f)), Value: []byte("foobarval")}
 	err = c.Set(malFormed)
 	if err != ErrMalformedKey {
 		t.Errorf("set(foo<0x7f>) should return ErrMalformedKey instead of %v", err)
